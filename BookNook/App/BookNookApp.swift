15332b7941f8ba70f79f00ce7eee67404009a1e7
freegatik <anton.soloviev2016@gmail.com>
2025-01-15 20:07:49 +0300

    SwiftUI app entry and root scene

---
+++ BookNook/App/BookNookApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookNookApp.swift
+//  BookNook
+//
+//  Created by Anton Solovev on 15.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct BookNookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainView()
+        }
+    }
+}
